2689b4596b1399398c789619dcee071d1f70d6e2
Murat MERCAN <mmercan@outlook.com>
2018-10-23 20:29:18 +1100

    prod compose added

---
+++ Sentinel.Web/prod.ps1
@@ -0,0 +1,9 @@
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$apiFolder = Join-Path -Path $dir -ChildPath .\dockapp
+
+
+
+#docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-sts sentinel-web-api-product sentinel-db-redis sentinel-db-mongodb sentinel-nats sentinel-web-api-comms
+docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-db-redis sentinel-db-mongodb sentinel-nats sentinel-web-sts sentinel-web-api-product sentinel-web-api-comms
+
