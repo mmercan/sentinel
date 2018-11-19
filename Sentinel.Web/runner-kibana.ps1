5d5d7f06716aa6dd8516d22ffe5299d251ff9218
Murat MERCAN <mmercan@outlook.com>
2018-11-19 17:24:53 +1100

    logstash added init

--- Sentinel.Web/runner-kibana.ps1
+++ Sentinel.Web/runner-kibana.ps1
@@ -40,7 +40,7 @@ $apiFolder = Join-Path -Path $dir -ChildPath .\dockapp
 
 #docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-sts sentinel-web-api-product sentinel-web-admin
 
-docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-db-elasticsearch sentinel-util-kibana
+docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-db-elasticsearch sentinel-util-kibana sentinel-elk-logstash
 
 
 
