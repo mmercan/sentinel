87ee1922039a9ddc36e86e0526b83649fa061c18
Murat MERCAN <mmercan@outlook.com>
2019-01-02 16:50:53 +1100

    ingress fun fixed

--- sentinel-ingress/run.ps1
+++ sentinel-ingress/run.ps1
@@ -6,7 +6,11 @@ helm install --name cert-manager01 --namespace kube-system stable/cert-manager -
 
 kubectl apply -f cluster-issuer-letsencrypt-prod.yaml
 
-kubectl apply -f certificates-prod-admin.yaml
-kubectl apply -f certificates-prod-product.yaml
 
-kubectl apply -f sentinel-admin-ingress.yaml
+kubectl apply -f sentinel-admin-cert-ingress-prod.yaml
+kubectl apply -f sentinel-auth-cert-ingress-prod.yaml
+kubectl apply -f sentinel-comms-cert-ingress-prod.yaml
+kubectl apply -f sentinel-kibana-cert-ingress-prod.yaml
+kubectl apply -f sentinel-member-cert-ingress-prod.yaml
+kubectl apply -f sentinel-product-cert-ingress-prod.yaml
+
