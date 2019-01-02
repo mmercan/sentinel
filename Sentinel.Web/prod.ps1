6800726fea9975a811b24f76db5db31bdbf84ea4
Murat MERCAN <mmercan@outlook.com>
2019-01-02 15:54:56 +1100

    nats container renamed

--- Sentinel.Web/prod.ps1
+++ Sentinel.Web/prod.ps1
@@ -3,10 +3,10 @@ $dir = Split-Path $scriptpath
 $apiFolder = Join-Path -Path $dir -ChildPath .\dockapp
 
 
-# docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-sts sentinel-api-product sentinel-db-redis sentinel-db-mongodb sentinel-services-nats sentinel-api-comms
+# docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-sts sentinel-api-product sentinel-db-redis sentinel-db-mongodb sentinel-service-nats sentinel-api-comms
 # docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-sts sentinel-web-admin
 # docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-web-admin
 
-docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-db-redis sentinel-db-mongodb sentinel-services-nats sentinel-web-sts sentinel-api-product sentinel-api-comms  sentinel-web-admin
+docker-compose -f docker-compose-prod-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-db-redis sentinel-db-mongodb sentinel-service-nats sentinel-web-sts sentinel-api-product sentinel-api-comms  sentinel-web-admin
 
 
--- Sentinel.Web/runner.ps1
+++ Sentinel.Web/runner.ps1
@@ -49,8 +49,8 @@ $apiFolder = Join-Path -Path $dir -ChildPath .\dockapp
 # docker-compose --verbose
 #docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-api-product sentinel-web-sts
 
-# docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-util-mailhog sentinel-db-mongodb sentinel-services-nats sentinel-db-redis sentinel-web-sts sentinel-api-product sentinel-api-comms sentinel-handler-comms sentinel-db-elasticsearch sentinel-util-kibana
-docker-compose -f docker-compose-debug-linux.yml up --build sentinel-util-mailhog sentinel-db-mongodb sentinel-services-nats sentinel-db-redis sentinel-web-sts sentinel-api-product sentinel-api-comms sentinel-handler-comms sentinel-db-elasticsearch sentinel-util-kibana
+# docker-compose -f docker-compose-debug-linux.yml up --build --force-recreate --renew-anon-volumes sentinel-util-mailhog sentinel-db-mongodb  sentinel-db-redis sentinel-web-sts sentinel-api-product sentinel-api-comms sentinel-handler-comms sentinel-db-elasticsearch sentinel-util-kibana
+docker-compose -f docker-compose-debug-linux.yml up --build sentinel-util-mailhog sentinel-db-mongodb sentinel-service-nats sentinel-db-redis sentinel-web-sts sentinel-api-product sentinel-api-comms sentinel-handler-comms sentinel-db-elasticsearch sentinel-util-kibana
 
 
 
