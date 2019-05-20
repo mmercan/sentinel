99a4e6d1245de4c999cbb339d506ea828c6e85ff
Murat MERCAN <mmercan@outlook.com>
2019-05-20 16:35:34 +1000

    namespace removed from kibana

---
+++ aks/install-virtual-kubelet.ps1
@@ -0,0 +1,12 @@
+$resouceGruoup = "akscluster-rg"
+$aksName = "aks-test"
+$clientSecret = "<Put the Secret>"
+$servicePrincipalGuid = "<Guid Number of AAD app witch can write to Resource Group>"
+
+# az aks create -g $resouceGruoup -n  $aksName --service-principal $servicePrincipalGuid --client-secret $clientSecret --location eastus --generate-ssh-keys
+# az aks install-cli
+# az aks get-credentials -g $resouceGruoup -n $aksName
+# kubectl apply -f rbac-virtual-kubelet.yaml
+# helm init --service-account tiller --upgrade
+# az aks install-connector --resource-group $resouceGruoup --name $aksName --connector-name virtual-kubelet --os-type Both
+# kubectl get nodes
