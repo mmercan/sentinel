5eca47648edc180c09a14ead9bbb7c9243959d6e
Murat MERCAN <mmercan@outlook.com>
2019-05-27 16:37:10 +1000

    avatar problems

--- Sentinel.Web/Sentinel.UI.HealthMonitoring/src/app/shared/authentication/user-avatar/user-avatar.component.spec.ts
+++ Sentinel.Web/Sentinel.UI.HealthMonitoring/src/app/shared/authentication/user-avatar/user-avatar.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { UserAvatarComponent } from "./user-avatar.component";
+import { UserAvatarComponent } from './user-avatar.component';
 
 describe('UserAvatarComponent', () => {
   let component: UserAvatarComponent;
