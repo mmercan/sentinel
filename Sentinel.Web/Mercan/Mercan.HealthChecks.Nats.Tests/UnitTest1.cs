6dfd6637fba64913623ca8274b9e9dfcfc3903ee
Murat MERCAN <mmercan@outlook.com>
2019-06-06 17:58:24 +1000

    tests added docker test changed to test sentinel.web.sln

---
+++ Sentinel.Web/Mercan/Mercan.HealthChecks.Nats.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Mercan.HealthChecks.Nats.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
