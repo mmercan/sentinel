2d3634ff5d9c34c531715cbfe03f213cf27ce024
Murat MERCAN <mmercan@outlook.com>
2019-07-29 23:48:51 +1000

    HealthcheckDataStoreSetService test added

---
+++ Sentinel.Web/Sentinel.UI.HealthMonitoring/src/app/shared/data-store/healthcheck-data-store/healthcheck-data-store-set.service.spec.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { AppConfig } from '../../../app.config';
+import { AdalService } from '../../authentication/adal-auth/adal.service';
+import { NotificationService } from '../../notification/notification.service';
+import { HealthcheckDataStoreSetService } from './healthcheck-data-store-set.service';
+
+describe('HealthcheckDataStoreService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [CommonModule, HttpClientModule],
+    providers: [NotificationService, AdalService, AppConfig, HealthcheckDataStoreSetService]
+  }));
+
+  it('should be created', () => {
+    const service: HealthcheckDataStoreSetService = TestBed.get(HealthcheckDataStoreSetService);
+    expect(service).toBeTruthy();
+  });
+});
