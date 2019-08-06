4fd5acf5d9617f9b67bc384e95ebfe692713b17b
Murat MERCAN <mmercan@outlook.com>
2019-08-06 12:45:41 +1000

    refactored

--- Sentinel.Web/HealthChecks/Mercan.HealthChecks.Network/HttpRequest/HttpRequestFactoryService.cs
+++ Sentinel.Web/HealthChecks/Mercan.HealthChecks.Network/HttpRequest/HttpRequestFactoryService.cs
@@ -12,13 +12,10 @@ namespace Mercan.HealthChecks.Network.HttpRequest
 {
     public class HttpRequestFactoryService
     {
-        public HttpClientOptions httpClientOptions;
-        private HttpRequestBuilder httpRequestBuilder;
+        public HttpClientOptions httpClientOptions { get; set; }
+        private readonly HttpRequestBuilder httpRequestBuilder;
 
         public ILogger<HttpRequestFactoryService> logger { get; set; }
-
-
-
         public HttpRequestFactoryService(IOptions<HttpClientOptions> httpClientOptions, HttpRequestBuilder httpRequestBuilder)
         {
             this.httpClientOptions = httpClientOptions.Value;
@@ -31,7 +28,6 @@ namespace Mercan.HealthChecks.Network.HttpRequest
             var builder = new HttpRequestBuilder()
                     .AddMethod(HttpMethod.Get)
                     .AddRequestUri(url)
-                    //.AddBearerToken(bearerToken)
                     .AddRequestContentType(httpClientOptions.RequestContentType)
                     .AddClientCertificate(httpClientOptions.ClientCertificateBase64)
                     .AddHeaders(httpClientOptions.DefaultRequestHeaders)
@@ -45,30 +41,7 @@ namespace Mercan.HealthChecks.Network.HttpRequest
             }
             catch (AggregateException ea)
             {
-                foreach (var ex in ea.Flatten().InnerExceptions)
-                {
-                    if (logger != null)
-                    {
-                        if (ex.InnerException != null && ex.InnerException.Message != null)
-                        {
-                            logger.LogError(ex.InnerException.Message + " Url : " + url, ex.InnerException);
-                        }
-                        else
-                        {
-                            logger.LogError(ex.Message + " Url : " + url, ex);
-                        }
-                    }
-                    if (ex is HttpRequestException)
-                    {
-                        if (ex.InnerException != null && ex.InnerException.Message == "The server name or address could not be resolved")
-                        {
-                            HttpResponseMessage respose = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
-                            respose.Content = new StringContent(ex.InnerException.Message + " url : " + url);
-                            return Tuple.Create(respose, url);
-                        }
-                    }
-                    throw ex;
-                }
+                HandleAggregateException(ea, url);
             }
             return Tuple.Create(taskresult.Result, url);
         }
@@ -94,33 +67,53 @@ namespace Mercan.HealthChecks.Network.HttpRequest
             }
             catch (AggregateException ea)
             {
-                foreach (var ex in ea.Flatten().InnerExceptions)
-                {
-                    if (logger != null)
-                    {
-
-                        if (ex.InnerException != null && ex.InnerException.Message != null)
-                        {
-                            logger.LogError(ex.InnerException.Message + " Url : " + url, ex.InnerException);
-                        }
-                        else
-                        {
-                            logger.LogError(ex.Message + " Url : " + url, ex);
-                        }
-                    }
-                    if (ex is HttpRequestException)
-                    {
-                        if (ex.InnerException != null && ex.InnerException.Message == "The server name or address could not be resolved")
-                        {
-                            HttpResponseMessage respose = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
-                            respose.Content = new StringContent(ex.InnerException.Message + " url : " + url);
-                            return Tuple.Create(respose, url);
-                        }
-                    }
-                    throw ex;
-                }
+                return HandleAggregateException(ea, url);
             }
             return Tuple.Create(taskresult.Result, url);
+        }
+
+
+        public Tuple<HttpResponseMessage, string> HandleAggregateException(AggregateException ea, string url)
+        {
+            foreach (var ex in ea.Flatten().InnerExceptions)
+            {
+                HttpResponseMessage respose = null;
+                string errormessage = ex?.InnerException?.Message;
+                Exception sendex = ex?.InnerException;
+                if (errormessage == null && ex.Message != null)
+                {
+                    errormessage = ex.Message;
+                    sendex = ex;
+                }
+                else
+                {
+                    errormessage = "InnerException.Message is Null";
+                }
+
+                if (logger != null)
+                {
+                    logger.LogError(errormessage + " Url : " + url, sendex);
+                }
+                if (ex is HttpRequestException)
+                {
+                    if (ex.InnerException != null && ex.InnerException.Message == "The server name or address could not be resolved")
+                    {
+                        respose = new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+                    }
+                    else
+                    {
+                        respose = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+                    }
+                }
+                if (respose == null)
+                {
+                    respose = new HttpResponseMessage(System.Net.HttpStatusCode.ServiceUnavailable);
+                }
+                respose.Content = new StringContent(errormessage + " url : " + url);
+                return Tuple.Create(respose, url);
+            }
+            HttpResponseMessage respose1 = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest); ;
+            return Tuple.Create(respose1, url);
         }
     }
 }
