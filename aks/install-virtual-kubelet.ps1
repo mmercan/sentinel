cb8c0224db5178b1957b155307d02e7556f62c95
Murat MERCAN <mmercan@outlook.com>
2019-08-08 21:35:22 +1000

    helm init

--- aks/install-virtual-kubelet.ps1
+++ aks/install-virtual-kubelet.ps1
@@ -1,9 +1,9 @@
 $resouceGruoup = "akscluster-rg"
-$aksName = "aks-test"
+$aksName = "aks-matt"
 $clientSecret = "<Put the Secret>"
 $servicePrincipalGuid = "<Guid Number of AAD app witch can write to Resource Group>"
 
-# az aks create -g $resouceGruoup -n  $aksName --service-principal $servicePrincipalGuid --client-secret $clientSecret --location eastus --generate-ssh-keys
+az aks create -g $resouceGruoup -n  $aksName --service-principal $servicePrincipalGuid --client-secret $clientSecret --location eastus --generate-ssh-keys
 # az aks install-cli
 # az aks get-credentials -g $resouceGruoup -n $aksName
 # kubectl apply -f rbac-virtual-kubelet.yaml
