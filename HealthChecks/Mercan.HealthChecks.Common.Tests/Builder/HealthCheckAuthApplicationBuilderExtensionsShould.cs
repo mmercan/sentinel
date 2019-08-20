2e2268fa836c34f2f62cc321df185d9dc1689633
Murat MERCAN <mmercan@outlook.com>
2019-08-20 15:27:07 +1000

    UseHealthChecksWithAuth test started

---
+++ HealthChecks/Mercan.HealthChecks.Common.Tests/Builder/HealthCheckAuthApplicationBuilderExtensionsShould.cs
@@ -0,0 +1,46 @@
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mercan.HealthChecks.Common.Checks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Mercan.HealthChecks.Common.Tests.Builder
+{
+    public class HealthCheckAuthApplicationBuilderExtensionsShould
+    {
+        [Fact]
+        public void AddtothePipelineWorks()
+        {
+            WebHost.CreateDefaultBuilder()
+            .ConfigureServices((cs) =>
+            {
+                cs.AddLogging();
+                cs.AddHealthChecks()
+                .AddSystemInfoCheck();
+            })
+            .Configure((app) =>
+            {
+                app.UseHealthChecksWithAuth("/isaliveandwell")
+                .UseHealthChecksWithAuth("/isaliveandwell", new HealthCheckOptions())
+                .UseHealthChecksWithAuth("/isaliveandwell", "4444")
+                .UseHealthChecksWithAuth("/isaliveandwell", 4445)
+                .UseHealthChecksWithAuth("/isaliveandwell", "4444", new HealthCheckOptions())
+                .UseHealthChecksWithAuth("/isaliveandwell", 4446, new HealthCheckOptions());
+
+            });
+
+
+
+
+        }
+    }
+}
