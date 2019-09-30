ac9d4323baddff0ba2aa7e29be928caf80de224c
Murat MERCAN <mmercan@outlook.com>
2019-09-30 14:50:11 +1000

    View and wwwroot removed

---
+++ Sentinel.Api.Billing/Helpers/ConfigureSwaggerOptions.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+
+namespace Sentinel.Api.Billing
+{
+
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "Sentinel.Api.Billing API",
+                Version = description.ApiVersion.ToString(),
+                Description = "Sentinel.Api.Billing, Swashbuckle, and API versioning.",
+                Contact = new OpenApiContact() { Name = "Matt Mercan", Email = "mmercan@outlook.com" },
+                TermsOfService = new Uri("https://mmercan.com"),
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
--- Sentinel.Api.Billing/Startup.cs
+++ Sentinel.Api.Billing/Startup.cs
@@ -1,265 +1,192 @@
 using System;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.Text;
-using Microsoft.AspNetCore.Authorization;
-using AutoMapper;
-using Serilog;
-using Serilog.Events;
-using Swashbuckle.AspNetCore.Swagger;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Sentinel.Common;
+using Serilog;
+using Serilog.Events;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using Mercan.HealthChecks.Common.Checks;
-using Mercan.HealthChecks.Mongo;
-using Mercan.HealthChecks.RabbitMQ;
-using Mercan.HealthChecks.Redis;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Mercan.HealthChecks.Common;
+using Mercan.HealthChecks.Mongo;
+using Mercan.HealthChecks.Redis;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Mercan.HealthChecks.RabbitMQ;
+using EasyNetQ;
+using Mercan.HealthChecks.Common.CheckCaller;
 
 namespace Sentinel.Api.Billing
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            this.Environment = environment;
         }
-
         public IConfiguration Configuration { get; }
-
-        public void ConfigureJwtAuthService(IServiceCollection services)
-        {
-            var audienceConfig = Configuration.GetSection("Tokens");
-            var symmetricKeyAsBase64 = audienceConfig["Secret"];
-            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
-            var signingKey = new SymmetricSecurityKey(keyByteArray);
-
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                // The signing key must match!
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = signingKey,
-                // Validate the JWT Issuer (iss) claim
-                ValidateIssuer = true,
-                ValidIssuer = audienceConfig["Issuer"],
-                // Validate the JWT Audience (aud) claim
-                ValidateAudience = true,
-                ValidAudience = audienceConfig["Audience"],
-                // Validate the token expiry
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero
-            };
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-
-
-
-          .AddJwtBearer("azure", cfg =>
-          {
-              cfg.RequireHttpsMetadata = false;
-              cfg.SaveToken = true;
-              cfg.Authority = Configuration["AzureAd:Instance"] + "/" + Configuration["AzureAD:TenantId"];
-              cfg.Audience = Configuration["AzureAd:ClientId"];
-          })
-            .AddJwtBearer("sts", cfg =>
-             {
-                 cfg.TokenValidationParameters = tokenValidationParameters;
-             });
-            //use both jwt schemas interchangeably  https://stackoverflow.com/questions/49694383/use-multiple-jwt-bearer-authentication
-            services.AddAuthorization(options =>
-            {
-                options.DefaultPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().AddAuthenticationSchemes("azure", "sts").Build();
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public IWebHostEnvironment Environment { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSingleton<IServiceCollection>(services);
             services.AddSingleton<IConfiguration>(Configuration);
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
 
-            services.AddHealthChecks()
-             .AddProcessList()
-             .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory")
-             .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory", "AvailableMBytes")
-             .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory", "PercentCommittedBytesInUse", "PercentCommittedBytesInUse_Base")
-             .AddSystemInfoCheck()
-            //.AddPrivateMemorySizeCheckMB(1000)
-            .AddWorkingSetCheckKB(250000)
+            var healthcheckBuilder = services.AddHealthChecks()
+               .AddProcessList()
+            //    .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory")
+            //    .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory", "AvailableMBytes")
+            //    .AddPerformanceCounter("Win32_PerfRawData_PerfOS_Memory", "PercentCommittedBytesInUse", "PercentCommittedBytesInUse_Base")
+               .AddSystemInfoCheck()
+              .AddWorkingSetCheckKB(450000)
+            //   .SqlConnectionHealthCheck(Configuration["SentinelConnection"])
+            //   .AddApiIsAlive(Configuration.GetSection("sentinel-api-member:ClientOptions"), "health/isalive")
+            //   .AddApiIsAlive(Configuration.GetSection("sentinel-api-product:ClientOptions"), "health/isalive")
+            //   .AddApiIsAlive(Configuration.GetSection("sentinel-api-comms:ClientOptions"), "health/isalive")
+            //   .AddMongoHealthCheck(Configuration["Mongodb:ConnectionString"])
+            //   .AddRedisHealthCheck(Configuration["RedisConnection"])
+              .AddDIHealthCheck(services);
 
-            .AddCheck<SlowDependencyHealthCheck>("Slow", failureStatus: null, tags: new[] { "ready", })
-            // .SqlConnectionHealthCheck(Configuration["SentinelConnection"])
-            // .AddApiIsAlive(Configuration.GetSection("sentinel-ui-sts:ClientOptions"), "health/isalive")
-            // .AddApiIsAlive(Configuration.GetSection("sentinel-api-member:ClientOptions"), "health/isalive")
-            // .AddApiIsAlive(Configuration.GetSection("sentinel-api-product:ClientOptions"), "health/isalive")
-            // .AddApiIsAlive(Configuration.GetSection("sentinel-api-comms:ClientOptions"), "health/isalive")
-             .AddMongoHealthCheck(Configuration["Mongodb:ConnectionString"])
-             .AddRabbitMQHealthCheck(Configuration["RabbitMQConnection"])
-             .AddRedisHealthCheck(Configuration["RedisConnection"])
-            .AddDIHealthCheck(services);
+            // if (Environment.EnvironmentName != "dockertest")
+            // {
+            //     healthcheckBuilder.AddRabbitMQHealthCheckWithDiIBus();
+            // }
 
+            services.AddApplicationInsightsTelemetry("15ce6ddc-8d32-418e-9d5c-eed1cd7d6096");
+            services.AddApplicationInsightsKubernetesEnricher();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSignalR();
-            ConfigureJwtAuthService(services);
+            services.ConfigureJwtAuthService(Configuration);
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
                 builder.AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .SetIsOriginAllowedToAllowWildcardSubdomains()
-                .AllowCredentials();
+                .SetIsOriginAllowedToAllowWildcardSubdomains();
+                //.AllowCredentials();
             }));
 
-            services.AddApiVersioning(o =>
+            services.AddApiVersioning(options =>
             {
-                o.AssumeDefaultVersionWhenUnspecified = true;
-                o.DefaultApiVersion = new ApiVersion(1, 0);
-                o.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
             });
             services.AddVersionedApiExplorer(options =>
             {
                 options.GroupNameFormat = "'v'VVV";
                 options.SubstituteApiVersionInUrl = true;
             });
-
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             services.AddSwaggerGen(options =>
             {
-                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
-                foreach (var description in provider.ApiVersionDescriptions)
+                options.OperationFilter<SwaggerDefaultValues>();
+                options.IncludeXmlComments(XmlCommentsFilePath);
+
+                options.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
                 {
-                    options.SwaggerDoc(
-                    description.GroupName,
-                    new Info()
-                    {
-                        Title = $"Sentinel.Api.Billing API {description.ApiVersion}",
-                        Version = description.ApiVersion.ToString()
-                    });
-                }
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "Bearer "
+                });
+
             });
+            services.AddSingleton<EasyNetQ.IBus>((ctx) =>
+            {
+                return RabbitHutch.CreateBus(Configuration["RabbitMQConnection"]);
+            });
+
+
+            services.AddHttpClient("run_with_try", options =>
+            {
+                options.Timeout = new TimeSpan(0, 2, 0);
+                options.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                options.DefaultRequestHeaders.Add("OData-Version", "4.0");
+                options.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            })
+            //.ConfigurePrimaryHttpMessageHandler<CertMessageHandler>()
+            // ConfigurePrimaryHttpMessageHandler((ch) =>
+            // {
+            //     var handler = new HttpClientHandler();
+            //     handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            //     handler.ClientCertificates.Add(HttpClientHelpers.GetCert());
+            //     return handler;
+
+            // })
+            // .AddHttpMessageHandler()
+            // .AddHttpMessageHandler<OAuthTokenHandler>()
+            //.AddHttpMessageHandler(*)
+            .AddPolicyHandler(HttpClientHelpers.GetRetryPolicy())
+            .AddPolicyHandler(HttpClientHelpers.GetCircuitBreakerPolicy());
+
+
+            // services.AddHostedService<HealthCheckSubscribeService>();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApiVersionDescriptionProvider provider)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IApiVersionDescriptionProvider provider)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            // app.UseSignalRJwtAuthentication();
+
             var logger = new LoggerConfiguration()
             .ReadFrom.Configuration(Configuration)
             .Enrich.FromLogContext()
-            .Enrich.WithProperty("Enviroment", env.EnvironmentName)
+            .Enrich.WithProperty("Enviroment", Environment.EnvironmentName)
             .Enrich.WithProperty("ApplicationName", "Api App")
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
             .WriteTo.Console()
             .WriteTo.File("Logs/logs.txt");
             //.WriteTo.Elasticsearch()
-
             logger.WriteTo.Console();
             loggerFactory.AddSerilog();
             Log.Logger = logger.CreateLogger();
             app.UseExceptionLogger();
-            // move  UseDefaultFiles to first line
-            // app.UseFileServer();
-            app.UseDefaultFiles();
-            app.UseSwagger(e =>
-            {
-                e.PreSerializeFilters.Add((doc, req) =>
-                {
-                    doc.Paths.Add("/Health/IsAliveAndWell", new PathItem
-                    {
-                        Get = new Operation
-                        {
-                            Tags = new List<string> { "HealthCheck" },
-                            Produces = new string[] { "application/json" },
-                            Responses = new Dictionary<string, Response>{
-                                {"200",new Response{Description="Success"}},
-                                {"503",new Response{Description="Failed"}}
-                            }
-                        }
-                    });
 
-                    doc.Paths.Add("/Health/IsAlive", new PathItem
-                    {
-                        Get = new Operation
-                        {
-                            Tags = new List<string> { "HealthCheck" },
-                            Produces = new string[] { "application/json" }
-                        }
-                    });
-                });
+            app.UseSwagger(e => { e.AddHealthCheckSwaggerOptions(); });
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+                options.RoutePrefix = string.Empty;
             });
 
-            app.UseSwaggerUI(options =>
-                {
-                    foreach (var description in provider.ApiVersionDescriptions)
-                    {
-                        options.SwaggerEndpoint(
-                            $"/swagger/{description.GroupName}/swagger.json",
-                            description.GroupName.ToUpperInvariant());
-                    }
-                });
             app.UseCors("MyPolicy");
-            app.UseCookiePolicy();
-            app.UseAuthentication();
-            // app.UseSignalR(routes =>
-            // {
-            //     routes.MapHub<ChatHub>("/hub/chat");
-            // });
-
-
-            // app.UseMvc(routes =>
-            // {
-            //     routes.MapRoute(
-            //         name: "default",
-            //         template: "{controller=Home}/{action=Index}/{id?}");
-            // });
-
+            app.UseRouting();
+            app.UseAllAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
-                      {
-                          endpoints.MapControllers();
-                      });
-
-            app.UseHealthChecks("/Health/IsAliveAndWell", new HealthCheckOptions()
             {
-                // This custom writer formats the detailed status as JSON.
+                endpoints.MapControllers();
+            });
+            // app.UseMiniProfiler();
+            app.UseHealthChecksWithAuth("/Health/IsAliveAndWell", new HealthCheckOptions()
+            {
                 ResponseWriter = WriteResponses.WriteListResponse,
             });
 
@@ -271,6 +198,17 @@ namespace Sentinel.Api.Billing
                     await context.Response.WriteAsync("{\"IsAlive\":true}");
                 });
             });
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                //var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var basePath = AppContext.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
         }
     }
 }
