c180a939d4b4fdbe6c21939c5247d60b0e3ffd3c
Murat MERCAN <mmercan@outlook.com>
2019-09-30 13:39:01 +1000

    profiling and home controller removed

--- Sentinel.Api.HealthMonitoring/Controllers/HomeController.cs
+++ Sentinel.Api.HealthMonitoring/Controllers/HomeController.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Sentinel.Api.HealthMonitoring.Models;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
+// using Sentinel.Api.HealthMonitoring.Models;
 
-namespace Sentinel.Api.HealthMonitoring.Controllers
-{
-    [Route("Home")]
-    // [Authorize(AuthenticationSchemes = "azure")]
+// namespace Sentinel.Api.HealthMonitoring.Controllers
+// {
+//     [Route("Home")]
+//     // [Authorize(AuthenticationSchemes = "azure")]
 
-    public class HomeController : Controller
-    {
-        [Authorize]
-        [Route("Index")]
-        public IActionResult Index()
-        {
-            return View();
-        }
+//     public class HomeController : Controller
+//     {
+//         [Authorize]
+//         [Route("Index")]
+//         public IActionResult Index()
+//         {
+//             return View();
+//         }
 
-        [Route("Privacy")]
-        public string Privacy()
-        {
-            var timezone = System.TimeZoneInfo.Local.StandardName;
-            return DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + " " + timezone + " Matt";
-        }
+//         [Route("Privacy")]
+//         public string Privacy()
+//         {
+//             var timezone = System.TimeZoneInfo.Local.StandardName;
+//             return DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + " " + timezone + " Matt";
+//         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("Error")]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         [Route("Error")]
+//         public IActionResult Error()
+//         {
+//             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//         }
+//     }
+// }
