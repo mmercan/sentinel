3ebb81bfa33e1b552ac80ccffc4e172c6da58e50
Murat MERCAN <mmercan@outlook.com>
2019-10-16 22:49:54 +1100

    AddDIObjectHealthCheck and test added

---
+++ HealthChecks/Mercan.HealthChecks.Common.Tests/Checks/DIObjectCheckerShould.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Threading.Tasks;
+using Mercan.HealthChecks.Common.Checks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Mercan.HealthChecks.Common.Tests.Checks
+{
+    public class DIObjectCheckerShould
+    {
+
+        private readonly ITestOutputHelper output;
+        HealthCheckContext context = new HealthCheckContext();
+        IServiceCollection services = new ServiceCollection();
+        ILoggerFactory factory = new LoggerFactory();
+        public DIObjectCheckerShould(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void CreateaDIObjectCheckerInstanceWithHealthly()
+        {
+            ILogger<DIObjectChecker<TestClass>> logger = new Logger<DIObjectChecker<TestClass>>(factory);
+            // var check = new DIHealthCheck(logger, services);
+            var myclass = new TestClass();
+            var services1 = new ServiceCollection()
+             .AddLogging();
+            services1.AddScoped<TestClass>();
+            services1.AddScoped<DIObjectChecker<TestClass>>((sp) =>
+            {
+                return new DIObjectChecker<TestClass>(logger, myclass, (tc) =>
+                {
+                    return tc.runrun() == true;
+                });
+            });
+
+            services1.AddHealthChecks().AddDIHealthCheck(services1);
+            var serviceProvider = services1.BuildServiceProvider();
+            var checker = serviceProvider.GetService<DIObjectChecker<TestClass>>();
+            var resultTask = checker.CheckHealthAsync(context);
+            resultTask.Wait();
+            output.WriteLine("Description" + resultTask.Result.Description);
+            Assert.True(resultTask.Result.Status == HealthStatus.Healthy);
+        }
+
+
+
+        [Fact]
+        public void CreateaDIObjectCheckerInstanceWithUnhealthly()
+        {
+            ILogger<DIObjectChecker<TestClass>> logger = new Logger<DIObjectChecker<TestClass>>(factory);
+            // var check = new DIHealthCheck(logger, services);
+            var myclass = new TestClass();
+            var services1 = new ServiceCollection()
+             .AddLogging();
+            services1.AddScoped<TestClass>();
+            services1.AddScoped<DIObjectChecker<TestClass>>((sp) =>
+            {
+                return new DIObjectChecker<TestClass>(logger, myclass, (tc) =>
+                {
+                    return tc.runrun() == false;
+                });
+            });
+
+            services1.AddHealthChecks().AddDIHealthCheck(services1);
+            var serviceProvider = services1.BuildServiceProvider();
+            var checker = serviceProvider.GetService<DIObjectChecker<TestClass>>();
+            var resultTask = checker.CheckHealthAsync(context);
+            resultTask.Wait();
+            output.WriteLine("Description" + resultTask.Result.Description);
+            Assert.True(resultTask.Result.Status == HealthStatus.Unhealthy);
+        }
+
+
+        [Fact]
+        public void CreateaDIObjectCheckerInstanceWithThrows()
+        {
+            ILogger<DIObjectChecker<TestClass>> logger = new Logger<DIObjectChecker<TestClass>>(factory);
+            // var check = new DIHealthCheck(logger, services);
+            var myclass = new TestClass();
+            var services1 = new ServiceCollection()
+             .AddLogging();
+            services1.AddScoped<TestClass>();
+            services1.AddScoped<DIObjectChecker<TestClass>>((sp) =>
+            {
+                return new DIObjectChecker<TestClass>(logger, myclass, (tc) =>
+                {
+                    throw new ApplicationException("Triggered exception");
+                });
+            });
+
+            services1.AddHealthChecks().AddDIHealthCheck(services1);
+            var serviceProvider = services1.BuildServiceProvider();
+            var checker = serviceProvider.GetService<DIObjectChecker<TestClass>>();
+            var resultTask = checker.CheckHealthAsync(context);
+            resultTask.Wait();
+            output.WriteLine("Description" + resultTask.Result.Description);
+            Assert.True(resultTask.Result.Status == HealthStatus.Unhealthy);
+        }
+
+
+        [Fact]
+        public async Task RunDIHealthCheckHealthCheck()
+        {
+
+            var services1 = new ServiceCollection()
+           .AddLogging();
+            services1.AddScoped<TestClass>();
+
+            services1.AddHealthChecks().AddDIObjectHealthCheck<TestClass>((tc) =>
+            {
+                return tc.runrun() == true;
+            });
+            var serviceProvider = services1.BuildServiceProvider();
+
+
+            var healthCheckService = serviceProvider.GetService<HealthCheckService>();
+            var result = await healthCheckService.CheckHealthAsync();
+            Assert.Equal(HealthStatus.Healthy, result.Status);
+        }
+
+    }
+
+
+    public class TestClass
+    {
+
+        public bool runrun()
+        {
+            return true;
+        }
+    }
+
+}
---
+++ HealthChecks/Mercan.HealthChecks.Common/Checks/HealthCheckBuilderExtensions.DIObjectChecker.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+
+namespace Mercan.HealthChecks.Common.Checks
+{
+    public static partial class HealthCheckBuilderExtensions
+    {
+        public static IHealthChecksBuilder AddDIObjectHealthCheck<T>(this IHealthChecksBuilder builder, Func<T, bool> testfunc)
+        {
+            //return builder.AddCheck($"DIHealthCheck", new DIHealthCheck(services));
+            var name = typeof(T).Name;
+            return builder.AddTypeActivatedCheck<DIObjectChecker<T>>($"DIObjectHealthCheck {name}", null, null, testfunc);
+        }
+    }
+
+    public class DIObjectChecker<T> : IHealthCheck
+    {
+
+        private ILogger<DIObjectChecker<T>> _logger;
+        T _myObject;
+        Func<T, bool> _testfunc;
+        public DIObjectChecker(ILogger<DIObjectChecker<T>> logger, T myObject, Func<T, bool> testfunc)
+        {
+            this._myObject = myObject;
+            this._logger = logger;
+            _testfunc = testfunc;
+
+            logger.LogCritical("DIHealthCheck Init");
+        }
+
+
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Task.Run(() =>
+            {
+
+                bool failedAny = false;
+                int order = 0;
+                IDictionary<string, Object> data = new Dictionary<string, object>();
+                data.Add("type", "DIHealthCheck");
+                try
+                {
+                    var result = _testfunc(_myObject);
+                    if (result)
+                    {
+                        string description = "DependencyInjection is healthy for all services";
+                        ReadOnlyDictionary<string, Object> rodata = new ReadOnlyDictionary<string, object>(data);
+                        return HealthCheckResult.Healthy(description, rodata);
+                    }
+                    else
+                    {
+                        string description = "DependencyInjection is healthy for all services";
+                        ReadOnlyDictionary<string, Object> rodata = new ReadOnlyDictionary<string, object>(data);
+                        return HealthCheckResult.Unhealthy(description, data: rodata);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string description = "DependencyInjection is failed with exception " + ex.Message;
+                    ReadOnlyDictionary<string, Object> rodata = new ReadOnlyDictionary<string, object>(data);
+                    return HealthCheckResult.Unhealthy(description, data: rodata);
+                }
+            });
+        }
+
+    }
+}
