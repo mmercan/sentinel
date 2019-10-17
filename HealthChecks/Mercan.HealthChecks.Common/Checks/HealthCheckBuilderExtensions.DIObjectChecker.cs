a4c8aa6f27c610d398e746b58fccc416ed117dac
Murat MERCAN <mmercan@outlook.com>
2019-10-17 15:52:52 +1100

    class cleaned

--- HealthChecks/Mercan.HealthChecks.Common/Checks/HealthCheckBuilderExtensions.DIObjectChecker.cs
+++ HealthChecks/Mercan.HealthChecks.Common/Checks/HealthCheckBuilderExtensions.DIObjectChecker.cs
@@ -13,7 +13,7 @@ namespace Mercan.HealthChecks.Common.Checks
     {
         public static IHealthChecksBuilder AddDIObjectHealthCheck<T>(this IHealthChecksBuilder builder, Func<T, bool> testfunc)
         {
-            var serviceCollection = builder.Services; //.BuildServiceProvider();
+            var serviceCollection = builder.Services;
 
             var name = typeof(T).Name;
             return builder.AddTypeActivatedCheck<DIObjectChecker<T>>($"DIObjectHealthCheck {name}", null, null, serviceCollection, testfunc);
@@ -23,27 +23,13 @@ namespace Mercan.HealthChecks.Common.Checks
     public class DIObjectChecker<T> : IHealthCheck
     {
         private readonly IServiceCollection _serviceCollection;
-        readonly ILogger _logger;
-        readonly T _myObject;
+        private readonly ILogger _logger;
         readonly Func<T, bool> _testfunc;
-        // public DIObjectChecker(ILogger<DIObjectChecker<T>> logger, T myObject, Func<T, bool> testfunc)
-        // {
-        //     this._myObject = myObject;
-        //     this._logger = logger;
-        //     _testfunc = testfunc;
-
-        //     logger.LogCritical("DIHealthCheck Init");
-        // }
-
-
         public DIObjectChecker(ILogger<DIObjectChecker<T>> logger, IServiceCollection serviceCollection, Func<T, bool> testfunc)
         {
-            //this._myObject = myObject;
             _serviceCollection = serviceCollection;
-            this._logger = logger;
+            _logger = logger;
             _testfunc = testfunc;
-
-            logger.LogCritical("DIHealthCheck Init");
         }
 
 
@@ -56,8 +42,8 @@ namespace Mercan.HealthChecks.Common.Checks
                 try
                 {
                     var sp = _serviceCollection.BuildServiceProvider();
-                    var _myObject = sp.GetRequiredService<T>();
-                    var result = _testfunc(_myObject);
+                    var myObject = sp.GetRequiredService<T>();
+                    var result = _testfunc(myObject);
                     if (result)
                     {
                         string description = "object is Healthy";
