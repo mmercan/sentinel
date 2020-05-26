86d2035d83f16a4b2ed094514afdd05ccedffb3f
Murat MERCAN <mmercan@outlook.com>
2020-05-26 20:37:29 +1000

    url fixed

--- Sentinel.UI.HealthMonitoring/src/environments/environment.prod.ts
+++ Sentinel.UI.HealthMonitoring/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  signalrBaseUrl: 'http://comms-api.dev.api.sentinel.mercan.io/',
+  signalrBaseUrl: 'http://comms-api.dev.api.sentinel.mercan.io',
   Nofitication: {
     publicKey: 'BCbYNxjxYPOcv3Hn8xZH1bB2kJLFLeO9Fx68U0C2FOZ7wFmG_yxGdiiNIWrFRHY6X1NL6egRgzZGAC_A_6fcigA',
     subscriptionRepoUrl: 'https://comms-api.dev.myrcan.com/api/PushNotification/subscribe',
